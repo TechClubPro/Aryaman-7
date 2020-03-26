fe14e6c5a9684155f8b3b7d807a14c6a7a0253c9
Aryaman-7 <62686179+Aryaman-7@users.noreply.github.com>
2020-03-26 12:59:05 +0530

    Add files via upload

---
+++ Special Greetings- Name.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 12:41:06 2020
+
+@author: Abhijit
+"""
+
+a=input("Enter your name:")
+
+if a=="Kabir" or a=="kabir" :
+    print("Hi Kabir, did you know that Kevin Durant got CoronaVirus?!")
+    
+elif a=="Atharva" or a=="atharva" :
+    print ("Whats up Atharva? How are you doing?")
+    
+elif a=="Miheer" or a=="miheer" :
+    print("Hello Miheer! Hope you are well.")
+    
+elif a=="Parthiv" or a=="parthiv" :
+    print ("How are you Parthiv?")
+    
+elif a=="Saurish"  or a=="saurish" :
+    print("How is it going Saurish?")
+    
+elif a=="Nikhil" or a=="nikhil " :
+    print ("Hi Nic, What's up?")
+    
+
+     
+          
